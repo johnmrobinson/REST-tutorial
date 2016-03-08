2da792369c58070d4fededb43ccabf5bb4fffb80
John Robinson <john.robinson@wellzesta.com>
2016-03-08 10:25:29 -0500

    finished part 2

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^', include('snippets.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
